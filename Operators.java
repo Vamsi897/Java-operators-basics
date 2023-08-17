0a34bb023ab4a8fe3ab8b50dbb40e123a7f3e508
Vamsi897 <126794623+Vamsi897@users.noreply.github.com>
2023-08-17 15:56:52 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,23 @@
+package Java basics;
+public class Operators
+{
+    public static void main(String[] args)
+    {
+        //java operators//
+        int var1=10,var2=20;
+        int add = var1+var2;
+        int subtraction = var1-var2;
+        int multiply = var1*var2 ;
+        int division =var1/var2 ;
+        int modulus = var1%var2 ;
+        int increement= ++var1;
+        int decreement = --var2;
+        System.out.println("sum="+add);
+        System.out.println("sub="+subtraction);
+        System.out.println("multiplication="+multiply);
+        System.out.println("Div="+division);
+        System.out.println("mod="+modulus);
+        System.out.println("increment="+increement);
+        System.out.println("decrement="+decreement);
+    }  
+}
